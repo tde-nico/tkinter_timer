8a1a58890bae42005168fedc3c44ae02a167b7d2
tde-nico <101901057+tde-nico@users.noreply.github.com>
2023-12-06 21:34:59 +0100

    init

---
+++ settings.py
@@ -0,0 +1,33 @@
+# size data
+CENTER_SIZE = 8
+LINE_WIDTH = 6
+FONT = 'Helvetica'
+BUTTON_FONT_SIZE = 20
+CLOCK_FONT_SIZE = 24
+LAP_ITEM_HEIGHT = 85
+
+# Framerate
+FPS = 60
+FRAMERATE = int(1000 / FPS)
+
+# background color
+BLACK = '#000'
+
+# button colors  
+ORANGE_DARK = '#332002'
+ORANGE_DARK_TEXT = '#ea9209'
+ORANGE_HIGHLIGHT = '#4a3006'
+GREEN = '#1b381e'
+GREEN_TEXT = '#6dba75'
+GREEN_HIGHLIGHT = '#2c5531'
+RED = '#331412'
+RED_TEXT = '#e5423d'
+RED_HIGHLIGHT = '#451e1c'
+
+# clock colors 
+ORANGE = '#FF9500'
+WHITE = '#FEFEFE'
+GREY = '#333'
+
+# lap colors 
+DARK_GREY = '#111'
